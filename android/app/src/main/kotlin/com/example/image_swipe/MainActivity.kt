b33867482f429d0d7247b4e10c02bffce19ba7d2
fajar ainul <fajarainul14@gmail.com>
2020-10-17 10:01:21 +0700

    init package flutter

--- android/app/src/main/kotlin/com/example/image_swipe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.image_swipe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/image_swipe.dart
@@ -0,0 +1,7 @@
+library image_swipe;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
