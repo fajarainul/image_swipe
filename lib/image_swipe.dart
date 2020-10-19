534b33104b974f558cd9292c86ee81bb0ab6c24a
fajar ainul <fajarainul14@gmail.com>
2020-10-19 16:10:11 +0700

    implement code from youtube tutorial devefy

--- lib/image_swipe.dart
+++ lib/image_swipe.dart
@@ -1,7 +1,56 @@
 library image_swipe;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:image_swipe/card_scroll_widget.dart';
+
+class ImageSwipe extends StatefulWidget{
+  List<String> images;
+
+  ImageSwipe({@required this.images});
+
+  @override
+  _ImageSwipeState createState() => _ImageSwipeState();
+}
+
+class _ImageSwipeState extends State<ImageSwipe> {
+  double currentPage;
+  PageController pageController;
+
+  _ImageSwipeState();
+
+  @override
+  void initState() {
+    super.initState();
+    currentPage = widget.images.length - 1.0;
+    pageController = PageController(initialPage: widget.images.length - 1);
+    pageController.addListener(() {
+      setState(() {
+        currentPage = pageController.page;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        CardScrollWidget(
+            currentPage,
+            widget.images,
+            12.0 / 16.0,
+            (12.0 / 16.0) * 1.2
+        ),
+        Positioned.fill(
+          child: PageView.builder(
+            itemCount: widget.images.length,
+            controller: pageController,
+            reverse: true,
+            itemBuilder: (context, index) {
+              return Container();
+            },
+          ),
+        )
+      ],
+    );
+  }
 }
